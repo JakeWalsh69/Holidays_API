7dfdc91c2929b5cff0588f03c431185100f64597
JakeWalsh69 <jrwalsh1997@gmail.com>
2019-03-15 13:17:50 +0000

    First Commit, created Holiday entity, added SqlServer, Entity Framework Tools, Migration and Database imports. Also added HolidayContext and Initial Create.

---
+++ Holiday_API/Holiday_API/Migrations/20190315125655_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Holiday_API.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Holidays",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Date = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Holidays", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Holidays");
+        }
+    }
+}
---
+++ Holiday_API/Holiday_API/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+// Authors: Jake Walsh and Jordan Williams
+
+namespace Holiday_API
+{
+    public class Holiday
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Date { get; set; }
+    }
+
+    public class HolidayContext : DbContext
+    {
+        public DbSet<Holiday> Holidays { get; set; }
+
+        // localDB connection string
+        // c:\users\gary\StudentDB1.mdf
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=HolidaysDB;Trusted_Connection=True;");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            using (HolidayContext holidayContext = new HolidayContext())
+            {
+                // add holidays
+                Holiday h1 = new Holiday() { Name = "Easter", Date = "Sunday, 21 April" };
+                Holiday h2 = new Holiday() { Name = "Fathers Day", Date = "Sunday, 16 June" };
+
+                holidayContext.Holidays.Add(h1);
+                holidayContext.Holidays.Add(h2);
+                holidayContext.SaveChanges();
+
+                var holidaysQueryInOrder = holidayContext.Holidays.OrderBy(holiday => holiday.ID);
+                foreach (var holiday in holidaysQueryInOrder)
+                {
+                    Console.WriteLine(holiday.ID + " " + holiday.Name + " " + holiday.Date);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
